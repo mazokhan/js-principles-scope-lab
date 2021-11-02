91e092740d43228b284813659663055236d682f4
mazen <mak9986@hotmail.com>
2021-11-02 14:06:22 +1100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+globalVar = Mazen
+
+function upperCaseCustomerName (){
+    customerName = "MAZEN";
+    return customerName
+}
+
+function setBestCustomer(){
+    bestcustomer = "Not bob";
+}
+
+function overwriteBestCustomer(){
+    bestcustomer = "Maybe bob";
+}
+
+function changeLeastFavoriteCustomer(){
+    leastfavouritecustomer = "john";
+}
